337783027ac90480c082f9cc41769e7d6e42b4d6
DataBells <sandeepsdfrance@gmail.com>
2025-01-30 20:39:10 +0100

    analyzing bronze data before coading for silver layer

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # data_warehouse_sql
+
 Building a modern data warehouse with SQL including ETL
+
+<br>
+
+Table Names <br>
+bronze.crm_cust_info <br>
+bronze.crm_prd_info <br>
+bronze.crm_sales_details <br>
+bronze.erp_cust_az12 <br>
+bronze.erp_loc_a101 <br>
+bronze.erp_px_cat_g1v2 <br>
 
 **Under Working** <br>
 Update Frequency: **Daily**
---
+++ scripts/silver/analyzing_bronze_data.sql
@@ -0,0 +1,38 @@
+-- analyzing first 1000 rows of data for each table in the bronze layer
+-- based on that we can develope integration model and help us for analyzing and understand data
+
+SELECT * FROM bronze.crm_cust_info
+LIMIT 1000;
+
+SELECT * FROM bronze.crm_prd_info
+LIMIT 1000;
+
+SELECT * FROM bronze.crm_sales_details
+LIMIT 1000;
+
+SELECT * FROM bronze.erp_cust_az12
+LIMIT 1000;
+
+SELECT * FROM bronze.erp_loc_a101
+LIMIT 1000;
+
+SELECT * FROM bronze.erp_px_cat_g1v2
+LIMIT 1000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
