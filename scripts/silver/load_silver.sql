fa83f56f0f8c705cecf1edc74c41761f268fe3c4
DataBells <sandeepsdfrance@gmail.com>
2025-01-30 22:53:54 +0100

    cleansing process for silver.crm_prd_info

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -34,3 +34,35 @@ from bronze.crm_cust_info
 )t where flag_last = 1; 
 
 select * from silver.crm_cust_info;
+
+insert into silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt 
+)
+select
+prd_id,
+replace(substring(prd_key, 1, 5), '-','_') as cat_id,
+substring(prd_key, 7, length(prd_key))as prd_key,
+prd_nm,
+coalesce(prd_cost, 0) AS prd_cost,
+case
+			when upper(trim(prd_line)) = 'R' then 'Road'
+			when upper(trim(prd_line)) = 'M' then 'Mountain'
+			when upper(trim(prd_line)) = 'S' then 'Other Sales'
+			when upper(trim(prd_line)) = 'T' then 'Touring'
+			else 'n/a'
+		end as prd_line,
+cast(prd_start_dt as DATE) as prd_start_dt,
+cast(
+    lead(prd_start_dt) over (partition by prd_key order by prd_start_dt) - interval '1 day' 
+    as DATE
+) as prd_end_dt
+from bronze.crm_prd_info;
+
+select * from silver.crm_prd_info;
