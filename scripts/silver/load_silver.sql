e1f7b7103acb8f4acf56e93dab92ab022628c2eb
DataBells <sandeepsdfrance@gmail.com>
2025-02-03 18:39:02 +0100

    load erp tables in silver

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -114,3 +114,56 @@ from bronze.crm_sales_details;
 -- check data
 select * from silver.crm_sales_details;
 
+-- load ERP table sin silver layer
+INSERT INTO silver.erp_cust_az12 (
+			cid,
+			bdate,
+			gen
+		)
+select
+CASE
+	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid)) -- Remove 'NAS' prefix if present
+	ELSE cid
+	END AS cid, 
+CASE
+	WHEN bdate > CURRENT_DATE THEN NULL
+	ELSE bdate
+	END AS bdate, -- Set future birthdates to NULL
+CASE
+	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+	ELSE 'n/a'
+	END AS gen
+from bronze.erp_cust_az12;
+
+SELECT * FROM SILVER.erp_cust_az12;
+
+INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+SELECT
+	REPLACE(cid, '-', '') AS cid, 
+	CASE
+	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+	END AS cntry -- Normalize and Handle missing or blank country codes
+FROM bronze.erp_loc_a101;
+SELECT * FROM silver.erp_loc_a101;
+
+INSERT INTO silver.erp_px_cat_g1v2 (
+			id,
+			cat,
+			subcat,
+			maintenance
+		)
+SELECT
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT * FROM bronze.erp_px_cat_g1v2;
